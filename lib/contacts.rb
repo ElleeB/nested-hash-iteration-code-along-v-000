0794f5b6d246464f2695960040ce241298572818
Laura Trager <laura.trager@gmail.com>
2017-11-02 17:21:37 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,10 +13,11 @@ require 'pry'
      }
    }
 
- contacts.each do |person, contact_details_hash|
-   contact_details_hash.each do |attribute, data|
-     if attribute == :favorite_ice_cream_flavors
-     binding.pry
+contacts.each do |person, contact_details_hash|
+  contact_details_hash.each do |attribute, data|
+    if attribute == :favorite_ice_cream_flavors
+      binding.pry
+    end
   end
 end
 
